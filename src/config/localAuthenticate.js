77b3b73dedb19be2a219d191472e28e10cefc9cd
Vlamir Santo <vlamir.santo@dotlib.com.br>
2020-05-11 19:15:05 -0300

    Autenticação local por meio da digital.

---
+++ src/config/localAuthenticate.js
@@ -0,0 +1,13 @@
+import {colors} from '~/styles';
+
+export default {
+  title: 'Autenticação requerida',
+  imageColor: colors.primary,
+  imageErrorColor: colors.red,
+  sensorDescription: null,
+  sensorErrorDescription: 'Falha',
+  cancelText: 'Cancel',
+  fallbackLabel: 'Show Passcode',
+  unifiedErrors: false,
+  passcodeFallback: false,
+};
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,46 +1,60 @@
-import React from 'react';
-import {View, Text, ImageBackground, StatusBar} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, ImageBackground, StatusBar, Alert} from 'react-native';
+import TouchID from 'react-native-touch-id';
 
 import Button from '~/components/Button';
+import config from '~/config/localAuthenticate';
 
 import {colors} from '~/styles';
 import styles from './styles';
 
 const background = require('~/assets/bg-login.png');
 
-const Login = (props) => (
-  <ImageBackground
-    source={background}
-    style={styles.container}
-    imageStyle={styles.background}>
-    <StatusBar barStyle="dark-content" />
+class Login extends Component {
+  _pressHandler = () => {
+    TouchID.authenticate('Toque no sensor para se autenticar.', config)
+      .then((success) => {
+        this.props.navigation.navigate('Main');
+      })
+      .catch((error) => console.tron.log);
+  };
 
-    <View style={styles.content}>
-      <Text style={styles.text}>Bem vindo ao</Text>
-      <Text style={styles.title}>Dirbbox</Text>
-      <Text style={styles.textDefault}>
-        Best cloud storage platform for all business and individuals to manage
-        there data Join For Free.
-      </Text>
-    </View>
+  render() {
+    return (
+      <ImageBackground
+        source={background}
+        style={styles.container}
+        imageStyle={styles.background}>
+        <StatusBar barStyle="dark-content" />
 
-    <View style={styles.wrapper}>
-      <Button
-        icon="fingerprint"
-        backgroundColor={colors.blueTransparent}
-        color={colors.blue}
-        title="Smart Id"
-        onPress={() => props.navigation.navigate('Main')}
-      />
-      <Button
-        icon="arrow-forward"
-        backgroundColor={colors.blue}
-        color={colors.white}
-        title="Login"
-        onPress={() => props.navigation.navigate('Main')}
-      />
-    </View>
-  </ImageBackground>
-);
+        <View style={styles.content}>
+          <Text style={styles.text}>Bem vindo ao</Text>
+          <Text style={styles.title}>Dirbbox</Text>
+          <Text style={styles.textDefault}>
+            Best cloud storage platform for all business and individuals to
+            manage there data Join For Free.
+          </Text>
+        </View>
+
+        <View style={styles.wrapper}>
+          <Button
+            icon="fingerprint"
+            backgroundColor={colors.blueTransparent}
+            color={colors.blue}
+            title="Smart Id"
+            onPress={this._pressHandler}
+          />
+          <Button
+            icon="arrow-forward"
+            backgroundColor={colors.blue}
+            color={colors.white}
+            title="Login"
+            onPress={() => this.props.navigation.navigate('Main')}
+          />
+        </View>
+      </ImageBackground>
+    );
+  }
+}
 
 export default Login;
